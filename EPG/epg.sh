3c7673124626f74e8c3ff4061065e1deeabdb45e
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-24 22:06:50 +0200

    Add files via upload

---
+++ EPG/epg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+npm install
+
+# Meo EPG
+
+npm run grab -- --site=meo.pt --output=../EPG/epg-meo-pt.xml --days=7
+
+# Orange EPG
+
+npm run grab -- --site=orange.fr --output=../EPG/epg-orange-fr.xml --days=7
+
+# Elisa Viihde EPG
+
+npm run grab -- --site=elisaviihde.fi --output=../EPG/epg-elizaviihde-fi.xml --days=7
+
+# Mi.tv EPG
+
+npm run grab -- --channels=sites/mi.tv/mi.tv_br.channels.xml --output=../EPG/epg-mitv-br.xml --days=7
+
+# Vodafone (DE) EPG
+
+npm run grab -- --site=vodafone.de --output=../EPG/epg-vodafone-de.xml --days=7
+
+# Compress EPG xml files
+
+xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
+
+# Remove EPG xml files
+
+rm epg*.xml
+
+exit 0
